ef293c26635716e3b6a2807fced83dfe573fdc27
Sergey Ryabov <ryabovs@yahoo.com>
2019-04-09 11:12:08 +0200

    add List styles

--- src/components/ui/Suggestions/index.tsx
+++ src/components/ui/Suggestions/index.tsx
@@ -1,4 +1,18 @@
 import React, {FunctionComponent} from 'react'
+import styled from 'styled-components'
+
+const List = styled.ul`
+    width: 400px;
+    margin: 0;
+    padding: ${(props) => props.theme.spacing.unit * 2}px
+        ${(props) => props.theme.spacing.unit * 4}px;
+    border: 1px solid ${(props) => props.theme.palette.primary};
+    font-size: ${(props) => props.theme.text.size.default};
+    font-family: inherit;
+    border-radius: 8px;
+    background-color: ${(props) => props.theme.palette.white};
+    list-style: none;
+`
 
 type Props = {
     suggestions: string[]
@@ -11,7 +25,7 @@ const Suggestions: FunctionComponent<Props> = ({suggestions}) => {
         ))
     }
 
-    return <ul>{renderSuggestions(suggestions)}</ul>
+    return <List>{renderSuggestions(suggestions)}</List>
 }
 
 export default Suggestions
